49011ce0acee57c9ccbd724742dc52e530826e29
Finbarr Brady <fibrady@cisco.com>
2015-06-15 13:56:18 +0100

    0.6

--- setup.py
+++ setup.py
@@ -8,12 +8,12 @@ from distutils.core import setup
 
 setup(
     name='hikvision',
-    version='0.5',
+    version='0.6',
     description='Provides a python interface to interact with a hikvision camera',
     author='Finbarr Brady',
     author_email='fbradyirl@users.noreply.github.com',
     url='https://github.com/fbradyirl/hikvision',
-    download_url = 'https://github.com/fbradyirl/hikvision/tarball/0.5',
+    download_url = 'https://github.com/fbradyirl/hikvision/tarball/0.6',
     keywords='hikvision camera python cgi interface',
     packages=['hikvision'],
     classifiers = [
