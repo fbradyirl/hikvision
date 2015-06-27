0fd44fd8d8afc0e07aa43a84f4efe02e3481839c
Finbarr Brady <finbarr@jellysms.com>
2015-06-28 00:01:07 +0100

    1.0

--- setup.py
+++ setup.py
@@ -8,19 +8,19 @@
 # git push --tags origin master
 # 
 # Upload to PyPI Live
-# python setup.py register -r pypi
-# python setup.py sdist upload -r pypi
+# python setup.py register -r pypi ; python setup.py sdist upload -r pypi
+# 
 
 from distutils.core import setup
 
 setup(
     name='hikvision',
-    version='0.9',
+    version='1.0',
     description='Provides a python interface to interact with a hikvision camera',
     author='Finbarr Brady',
     author_email='fbradyirl@users.noreply.github.com',
     url='https://github.com/fbradyirl/hikvision',
-    download_url = 'https://github.com/fbradyirl/hikvision/tarball/0.9',
+    download_url = 'https://github.com/fbradyirl/hikvision/tarball/1.0',
     keywords='hikvision camera python cgi interface',
     packages=['hikvision'],
     classifiers = [
