de652cc6085c72eb95febaf5ebc62b45fa4ed49b
Finbarr Brady <fibrady@cisco.com>
2020-09-07 11:37:24 +0100

    Bump version: 2.0.0 → 2.0.1

--- hikvision/__init__.py
+++ hikvision/__init__.py
@@ -4,4 +4,4 @@
 
 __author__ = """Finbarr Brady"""
 __email__ = 'fbradyirl@github.io'
-__version__ = '2.0.0'
+__version__ = '2.0.1'
--- setup.py
+++ setup.py
@@ -38,6 +38,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/fbradyirl/hikvision',
-    version='2.0.0',
+    version='2.0.1',
     zip_safe=False,
 )
