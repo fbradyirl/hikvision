fca6fd7ae29b04233a06314f9e00552279b79a45
Finbarr Brady <fibrady@cisco.com>
2020-09-07 11:55:49 +0100

    Bump version: 2.0.1 → 2.0.2

--- setup.py
+++ setup.py
@@ -35,6 +35,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/fbradyirl/hikvision',
-    version='2.0.1',
+    version='2.0.2',
     zip_safe=False,
 )
